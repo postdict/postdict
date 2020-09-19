a9807e0532fa81d7493d1bda883446c3c7717eae
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2020-09-20 07:21:31 +0900

    Make sure the uniqueness of email and user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ApplicationRecord
   validates :family_name, { presence: true }
   validates :first_name, { presence: true }
-  validates :email, { presence: true }
+  validates :email, { presence: true, uniqueness: true }
   has_secure_password
 
   def User.generate_user_id
+    new_user_id = User.generate_new_id
+    # Retry generation of user_id for a new user if it matches other's one
+    while User.find_by(user_id: new_user_id)
+      new_user_id = User.generate_new_id
+    end
+
+    return new_user_id
+  end
+
+  private
+
+  def User.generate_new_id
     return ((0..9).to_a + ('a'..'z').to_a + ('A'..'Z').to_a).sample(12).join
   end
 end
