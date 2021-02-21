06865bbab38234cb9aa8830abfb70881fb076282
Ko001 <syapen4bkoji@gmail.com>
2021-02-21 20:35:44 +0900

    change view according to noun row table

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -19,7 +19,7 @@ class SearchController < ApplicationController
     @prequizzes = Quiz.where(original_noun: params[:noun], kind: "pre")
     @postquizzes = Quiz.where(original_noun: params[:noun], kind: "post")
 
-    noun_form_names = [
+    @noun_form_names = [
       "__ V",
       "__ BE",
       "V __",
@@ -29,7 +29,22 @@ class SearchController < ApplicationController
       "名詞句のみ"
     ]
 
-    
+    # @noun_rows = []
+    # noun_form_names.each do |noun_form_name|
+    #   @noun_rows << @noun.noun_rows.find_by(noun_form_name: noun_form_name)
+    # end
+    @noun_rows = Array.new(7)
+    for k in 0..6
+      @noun_rows[k] = Array.new(4)
+    end
+
+    for i in 0..6
+        for j in 0..3
+          if(@noun.noun_rows.where(row_num: i, column_num: j).present?)
+            @noun_rows[i][j] = @noun.noun_rows.where(row_num: i, column_num: j)
+          end
+        end
+    end
   end
 
 end
